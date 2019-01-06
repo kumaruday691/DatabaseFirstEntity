9a4ebf1fa8c1e46ff43f1e949a7a5db72cf19936
Uday Tori <kumaruday691@gmail.com>
2019-01-05 19:23:46 -0600

    testing framework - not much code

--- DatabaseFirstEntity/Program.cs
+++ DatabaseFirstEntity/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DatabaseFirstEntity
@@ -10,6 +12,14 @@ namespace DatabaseFirstEntity
     {
         static void Main(string[] args)
         {
+            PlutoDbContext dbContext = new PlutoDbContext();
+            var courses = dbContext.GetCourses();
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Description);
+                Thread.Sleep(5000);
+            }
+
         }
     }
 }
